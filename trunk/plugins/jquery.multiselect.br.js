23f2fdde7e26c8fbe67e0dc842a9be0273a46b11
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-03-16 18:35:37 +0000

    estrutura

---
+++ trunk/plugins/jquery.multiselect.br.js
@@ -0,0 +1,9 @@
+/* Brazilian initialisation for the jQuery UI multiselect plugin. */
+/* Written by Vinícius Fontoura Corrêa (vinusfc@gmail.com). */
+
+$.extend($.ech.multiselect.prototype.options, {
+	checkAllText: 'Marcar',
+	uncheckAllText: 'Desmarcar',
+	noneSelectedText: 'Selecione as op&ccedil;&otilde;es',
+	selectedText: '# selecionado'
+});
