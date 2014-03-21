1cdd8e3e347b296c38adf5f2a142eed29409e405
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-03-21 23:35:27 +0000

    Cadastro Acessibilidade

--- trunk/adm/includes/header.php
+++ trunk/adm/includes/header.php
@@ -56,6 +56,7 @@
                     	<a href="imprensaLista.php" style="background-image:url(imgs/icon-videos.png);">Imprensa</a><br />
 		           		<a href="acessibilidadeEdicao.php?texto_id=2" style="background-image:url(imgs/icon-blog.png);">Acessibilidade</a><br />
                         <a href="newsletterLista.php" style="background-image:url(imgs/icon-jogos.png);">Newsletter</a><br />
+                        <a href="emktLista.php" style="background-image:url(imgs/icon-jogos.png);">E-mail MKT</a><br />
                         <a href="contatosLista.php" style="background-image:url(imgs/icon-blog.png);">Contatos</a>
                     </div>
                 </span>
