9d6a1188dfde27aef7c9aa8e5020c87ba1e62046
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-03-21 22:56:49 +0000

    Cadastro Quem Somos

---
+++ trunk/adm/js/quemSomos.js
@@ -0,0 +1,12 @@
+(function($){
+	$(document).ready(function(){
+		$("#salvar").click(function(){
+			if(verifyObrigatorioForm("#formSalvar")){
+				$("#formSalvar").submit();
+			}else{
+				newAlert("Preencha os campos em destaque");
+			}
+		});
+
+	});
+})(jQuery);
