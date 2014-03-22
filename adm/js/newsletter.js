1b3451ab9a2121cf763a7d7306b072b2da562643
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-03-22 02:02:02 +0000

    Cadastro de Newsletter

---
+++ adm/js/newsletter.js
@@ -0,0 +1,38 @@
+(function($){
+	$(document).ready(function(){
+		$("#filtrar").click(function(){
+			$("#formbusca").submit();
+		});
+		$("#salvar").click(function(){
+			if(verifyObrigatorioForm("#formSalvar")){
+				$("#formSalvar").submit();
+			}else{
+				newAlert("Preencha os campos em destaque");
+			}
+		});
+		$(".btDel").live('click',function(){
+			var obj = $(this);
+			var id = trim($(this).attr('rel'));
+			var param = {
+				'newsletter_id':id
+			}
+			deleteItem(
+					"Deseja excluir esse item?"
+					,"controller/newsletter.controller.php?action=deleteItem"
+					,param
+					,function(msg,oDialog){
+						oDialog.dialog('close');
+						oDialog.dialog('destroy');
+						obj.parent().parent().remove();
+						newAlert(msg);
+					}
+					,function(msg,oDialog){
+						oDialog.dialog('close');
+						oDialog.dialog('destroy');
+						newAlert(msg);
+					}
+			)
+		});
+	});
+})(jQuery);
+
