22036a13d433e5229af97efa606c1c21a71cbacf
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-03-22 15:30:13 +0000

    Correção de cadastro

--- adm/includes/header.php
+++ adm/includes/header.php
@@ -55,7 +55,7 @@
                     	<a href="downloadsLista.php" style="background-image:url(imgs/icon-jogos.png);">Downloads</a><br />
                     	<a href="imprensaLista.php" style="background-image:url(imgs/icon-videos.png);">Imprensa</a><br />
 		           		<a href="acessibilidadeEdicao.php?texto_id=2" style="background-image:url(imgs/icon-blog.png);">Acessibilidade</a><br />
-                        <a href="newsletterLista.php" style="background-image:url(imgs/icon-jogos.png);">Newsletter</a><br />
+                        <a href="mailingLista.php" style="background-image:url(imgs/icon-jogos.png);">Mailing</a><br />
                         <a href="emktLista.php" style="background-image:url(imgs/icon-jogos.png);">E-mail MKT</a><br />
                         <a href="contatosLista.php" style="background-image:url(imgs/icon-blog.png);">Contatos</a>
                     </div>
