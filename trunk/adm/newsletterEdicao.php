a21beec075f90d62199a938e73ed5ab5f94ffd57
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-03-22 02:07:09 +0000

    Cadastro de Newsletter

--- trunk/adm/newsletterEdicao.php
+++ trunk/adm/newsletterEdicao.php
@@ -14,7 +14,7 @@
 		$aErro['erro'] =  $erro;
 		$obj->unRegisterSession($aErro);
 	}
-	$obj->debub($aRow);
+	$obj->debug($aRow);
 ?>
 <script type="text/javascript" src="js/newsletter.js"></script>
 <div id="contentWrapper">
@@ -34,8 +34,7 @@
 				</tr>
 				<tr>
 					<td colspan="3">
-						Receber Informações?<br />
-						<input type="checkbox" name="newsletter_receber_informacoes" value="S" <?=$aRow['newsletter_receber_informacoes']=='S'?' ckecked':''?>>Receber Informações<br>
+						<input type="checkbox" name="newsletter_receber_informacoes" value="S" <?=$aRow['newsletter_receber_informacoes']=='S' ? ' ckecked':''?>>&nbsp;Receber Informações<br>
 					</td>
 				</tr>
 				<tr>
