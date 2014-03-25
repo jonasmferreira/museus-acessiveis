673d639574d470fee6e3976389250cb048609f3d
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-03-25 13:39:57 +0000

    adicionando meta tags

--- trunk/head.php
+++ trunk/head.php
@@ -8,6 +8,10 @@
 	$linkAbsolute=$aConfig['configuracao_baseurl'];
 	$seqAleatoria = "rnd=".str_replace(".","",microtime(true));
 
+	$author = $aConfig['configuracao_meta_author'];
+	$keywords = $aConfig['configuracao_meta_keywords'];
+	$description = $aConfig['configuracao_meta_description'];
+	
 	//itens dos contatos no footer
 	include_once("{$path_root_page}adm{$DS}class{$DS}contato.class.php");
 	$objContato = new contato();
@@ -23,6 +27,11 @@
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Museus Acessíveis</title>
+
+<meta name="description" content="<?=$description;?>">
+<meta name="keywords" content="<?=$keywords;?>">
+<meta name="author" content="<?=$author;?>">
+
 <link rel="shortcut icon" href="<?=$linkAbsolute?>img/favicon.ico?<?=$seqAleatoria?>" type="image/x-icon" />
 <link rel="stylesheet" href="<?=$linkAbsolute?>plugins/lightbox/css/lightbox.css?<?=$seqAleatoria?>" type="text/css" media="screen" />
 <link rel="stylesheet" href="<?=$linkAbsolute?>css/style.css?<?=$seqAleatoria?>" type="text/css" media="screen" />
