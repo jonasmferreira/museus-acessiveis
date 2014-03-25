c7cfa5a57d0b6c1f3b23f39f58ab9177fb7b63c8
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-03-25 16:14:36 +0000

    novos links

--- menu.php
+++ menu.php
@@ -2,18 +2,18 @@
         	<ul id="principal">
             	<li class="fontSize"><a tabIndex="5" href="<?=$linkAbsolute;?>home">home</a></li>
             	<li class="fontSize"><a tabIndex="6" href="<?=$linkAbsolute;?>quem_somos">quem somos</a></li>
-            	<li class="fontSize"><a tabIndex="7" href="<?=$linkAbsolute;?>servico_lista">serviços</a></li>
-            	<li class="sub-mn fontSize"><a tabIndex="8" href="<?=$linkAbsolute;?>projeto_lista">projetos</a>
+            	<li class="fontSize"><a tabIndex="7" href="<?=$linkAbsolute;?>servicos">serviços</a></li>
+            	<li class="sub-mn fontSize"><a tabIndex="8" href="<?=$linkAbsolute;?>projetos">projetos</a>
                     <div class="submenu">
                         <ul>
-                            <li class="fontSize"><a tabIndex="9" href="<?=$linkAbsolute;?>abertos.html">Projetos abertos para captação</a></li>
-                            <li class="fontSize"><a tabIndex="10" href="<?=$linkAbsolute;?>realizados.html">Portifólio de projetos realizados</a></li>
-                            <li class="fontSize"><a tabIndex="11" href="<?=$linkAbsolute;?>andamento.html">Projetos em andamento</a></li>
+                            <li class="fontSize"><a tabIndex="9" href="<?=$linkAbsolute;?>projetos/abertos">Projetos abertos para captação</a></li>
+                            <li class="fontSize"><a tabIndex="10" href="<?=$linkAbsolute;?>projetos/realizados">Portifólio de projetos realizados</a></li>
+                            <li class="fontSize"><a tabIndex="11" href="<?=$linkAbsolute;?>projetos/em_andamento">Projetos em andamento</a></li>
                        </ul>
                     </div>
                 </li>
-            	<li class="fontSize"><a tabIndex="12" href="<?=$linkAbsolute;?>curso_lista.html">cursos</a></li>
-            	<li class="fontSize"><a tabIndex="13" href="<?=$linkAbsolute;?>download.html">downloads</a></li>
-            	<li class="fontSize"><a tabIndex="14" href="<?=$linkAbsolute;?>imprensa.html" style="padding-right:none !important;">imprensa</a></li>
+            	<li class="fontSize"><a tabIndex="12" href="<?=$linkAbsolute;?>cursos">cursos</a></li>
+            	<li class="fontSize"><a tabIndex="13" href="<?=$linkAbsolute;?>downloads">downloads</a></li>
+            	<li class="fontSize"><a tabIndex="14" href="<?=$linkAbsolute;?>imprensa" style="padding-right:none !important;">imprensa</a></li>
             </ul>
         </div>
