44cc88306431ed3bcc8a1c0c7d1de2f14eaad809
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-03-26 14:46:58 +0000

    Glossario

--- trunk/boxGlossario.php
+++ trunk/boxGlossario.php
@@ -1,9 +1,54 @@
-        <form action="" method="POST" id="glossary-search">
+<?php
+
+	//itens dos contatos no footer
+	include_once("{$path_root_page}adm{$DS}class{$DS}glossario.class.php");
+	$objGlossario = new glossario();
+	$objGlossario->setValues(array(
+		'glossario_exibir'=>'S'
+		,'page'=>'1'
+		,'rows'=>'500000'
+	));
+	
+	$objGlossario->setAOrderBy(array(
+		't.glossario_palavra' => 'ASC'
+	));
+	
+	
+	$aRows = $objGlossario->getLista();
+	//$objGlossario->debug($aRows);
+	
+?>
+
+		<div id="glossary-search">
         	<h1 tabIndex="110">Glossário da <br />Acessibilidade</h1>
             <p tabIndex="111" class="description">
             Saiba mais sobre inclusãosem complicação!!!
             </p>
-            <input tabIndex="112" type="text" class="field" value="" />
-			<input tabIndex="113" type="image" class="bt-search" src="img/search-bt_transparent.png" style="" />
-            <div class="clear"></div>
-        </form>
+			<div class="clear"></div>
+            <div class="tag-box">
+			<?php
+				$sLetra = '';
+				$sCloseUl = '';
+				foreach($aRows['rows'] as $k => $v){
+					if($sLetra != substr($v['glossario_palavra'],0,1)){
+						$sLetra=substr($v['glossario_palavra'],0,1);
+						if(trim($sCloseUl)!=''){
+							echo '</ul>';
+							echo '<h3>'.strtoupper($sLetra).'</h3>';
+							echo '<ul class="tag">';
+						}else{
+							$sCloseUl='_';	
+							echo '<h3>'.$sLetra.'</h3>';
+							echo '<ul class="tag">';
+						}
+?>
+				            <li><a href="<?=$linkAbsolute;?>glossario/<?=$v['glossario_id'];?>/<?=$objGlossario->toNormaliza($v['glossario_palavra']);?>"><?=$v['glossario_palavra'];?></a></li>
+<?php				
+					}
+				}
+			
+			?>
+            </div>
+			<div class="clear"></div>
+        </div>
+		<div class="clear"></div>
