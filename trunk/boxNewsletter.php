6e2b6d7ea57cf359f5dd3bb7f58c298b7904acb6
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-03-31 22:16:23 +0000

    Fique atento

--- trunk/boxNewsletter.php
+++ trunk/boxNewsletter.php
@@ -3,7 +3,8 @@
             <p tabIndex="107" class="description">
             Cadastre-se para recebernossas novidades!!!
             </p>
-            <input tabIndex="108" name="mailing_nome" id="mailing_nome" type="text" class="field" value="Nome" /><br />
+            <div><input tabIndex="108" name="mailing_nome" id="mailing_nome" type="text" class="field" value="Nome" /></div>
+			<div class="clear" style="height:3px;"></div>
             <input tabIndex="108" name="mailing_email" id="mailing_email" type="text" class="field" value="E-mail" />
 			<input tabIndex="109" type="image" class="bt-newsletter" src="<?=$linkAbsolute?>img/newsletter-bt_transparent.png" /><div class="clear"></div>
         </form>
