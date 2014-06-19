7c6c62e4150bbede42911a8301c4537089a52a5b
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-06-19 17:24:04 +0000

    Correção de bugs

--- adm/includes/header.php
+++ adm/includes/header.php
@@ -70,6 +70,7 @@
 						<a href="tipo_servicoLista.php" style="background-image:url(imgs/icon-jogos.png);">Tipos de Serviço</a><br />
                     	<a href="servicoLista.php" style="background-image:url(imgs/icon-jogos.png);">Serviços</a><br />
                     	<a href="projetoLista.php" style="background-image:url(imgs/icon-jogos.png);">Projetos</a><br />
+						<a href="servProjCurInfoEdicao.php?serv_proj_cur_id=1" style="background-image:url(imgs/icon-jogos.png);">Descrição Produtos</a><br />                        
 						<a href="extraLista.php" style="background-image:url(imgs/icon-jogos.png);">Informações Extras</a>                        
                     </div>
                 </span>                
