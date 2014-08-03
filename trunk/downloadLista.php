2b84cbd4727be27cc423c88b89d1672a395aee9a
inboxfox@gmail.com <inboxfox@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-08-03 15:42:52 +0000

    Testes de Commit

--- trunk/downloadLista.php
+++ trunk/downloadLista.php
@@ -1,8 +1,7 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<?php 
-
+	<?php
 		$path_root_page = dirname(__FILE__);
 		$DS = DIRECTORY_SEPARATOR;
 		$path_root_page = "{$path_root_page}{$DS}";
