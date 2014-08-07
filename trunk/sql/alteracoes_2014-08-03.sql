82e9adbae226315e9437d5b51813950b652fffa2
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-08-07 12:58:27 +0000

    Newsletter

--- trunk/sql/alteracoes_2014-08-03.sql
+++ trunk/sql/alteracoes_2014-08-03.sql
@@ -1,3 +1,4 @@
+/*
 DROP TABLE IF EXISTS `tb_emailmkt`;
 CREATE TABLE IF NOT EXISTS `tb_emailmkt` (
   `emailmkt_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -38,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `tb_emailmkt_conferencia` (
   PRIMARY KEY (`emailmkt_conferencia_id`));
   
   
-ALTER TABLE `museus_acessiveis`.`tb_emailmkt_conferencia` 
+ALTER TABLE `tb_emailmkt_conferencia` 
 ADD INDEX `idx_emailmkt_conferencia_01` (`mailing_email` ASC),
 ADD INDEX `fk_emailmkt_conferencia_01_idx` (`emailmkt_id` ASC),
 ADD INDEX `fk_emailmkt_conferencia_02_idx` (`mailing_id` ASC);
@@ -54,6 +55,52 @@ ADD CONSTRAINT `fk_emailmkt_conferencia_02`
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
-
+*/
   
-   
+
+DROP TABLE IF EXISTS `tb_emailmkt_conferencia`;
+DROP TABLE IF EXISTS `tb_emailmkt`;
+
+CREATE TABLE  `tb_emailmkt` (
+  `emailmkt_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `emailmkt_titulo` varchar(255) NOT NULL,
+  `emailmkt_qtde_enviada` int(11) NOT NULL,
+  `emailmkt_dt_agendada` date NOT NULL,
+  `emailmkt_hr_agendada` time NOT NULL,
+  `emailmkt_dt_disparo` date DEFAULT NULL,
+  `emailmkt_hr_disparo` time DEFAULT NULL,
+  `emailmkt_status` enum('P','X','E','C') NOT NULL DEFAULT 'P',
+  `emailmkt_servico_ids` text NOT NULL,
+  `emailmkt_projeto_ids` text NOT NULL,
+  `emailmkt_glossario_ids` text NOT NULL,
+  `emailmkt_novidade360_id` bigint(20) NOT NULL,
+  `emailmkt_novidade360_ids` text NOT NULL,
+  `emailmkt_agenda_ids` text NOT NULL,
+  `emailmkt_arq_fisico` text NOT NULL,
+  `emailmkt_aqui_tem_titulo` text NOT NULL,
+  `emailmkt_aqui_tem_resumo` text NOT NULL,
+  `emailmkt_aqui_tem_thumb` text NOT NULL,
+  `emailmkt_aqui_tem_url` text NOT NULL,
+  `emailmkt_propaganda_img` text NOT NULL,
+  PRIMARY KEY (`emailmkt_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+
+
+CREATE TABLE  `tb_emailmkt_conferencia` (
+  `emailmkt_conferencia_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `emailmkt_id` int(10) unsigned NOT NULL,
+  `mailing_id` bigint(20) unsigned NOT NULL,
+  `mailing_email` varchar(255) NOT NULL,
+  `emailmkt_conferencia_dt_disparo` date DEFAULT NULL,
+  `emailmkt_conferencia_hr_disparo` time DEFAULT NULL,
+  `emailmkt_conferencia_dt_visualizacao` date DEFAULT NULL,
+  `emailmkt_conferencia_hr_visualizacao` time DEFAULT NULL,
+  PRIMARY KEY (`emailmkt_conferencia_id`),
+  KEY `idx_emailmkt_conferencia_01` (`mailing_email`),
+  KEY `fk_emailmkt_conferencia_01_idx` (`emailmkt_id`),
+  KEY `fk_emailmkt_conferencia_02_idx` (`mailing_id`),
+  CONSTRAINT `fk_emailmkt_conferencia_01` FOREIGN KEY (`emailmkt_id`) REFERENCES `tb_emailmkt` (`emailmkt_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_emailmkt_conferencia_02` FOREIGN KEY (`mailing_id`) REFERENCES `tb_mailing` (`mailing_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
