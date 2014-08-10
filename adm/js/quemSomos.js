3d6468f14ea132004c695664d90db460e38bb5ae
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-08-10 18:25:53 +0000

    Implementações / Correções

--- adm/js/quemSomos.js
+++ adm/js/quemSomos.js
@@ -1,5 +1,8 @@
 (function($){
 	$(document).ready(function(){
+		$("#filtrar").click(function(){
+			$("#formbusca").submit();
+		});
 		$("#salvar").click(function(){
 			if(verifyObrigatorioForm("#formSalvar")){
 				$("#formSalvar").submit();
@@ -7,6 +10,66 @@
 				newAlert("Preencha os campos em destaque");
 			}
 		});
-
+		$(".btDel").live('click',function(){
+			var obj = $(this);
+			var id = trim($(this).attr('rel'));
+			var param = {
+				'quemsomos_id':id
+			}
+			deleteItem(
+					"Deseja excluir esse Item?"
+					,"controller/quemSomos.controller.php?action=deleteItem"
+					,param
+					,function(msg,oDialog){
+						oDialog.dialog('close');
+						oDialog.dialog('destroy');
+						obj.parent().parent().remove();
+						newAlert(msg);
+					}
+					,function(msg,oDialog){
+						oDialog.dialog('close');
+						oDialog.dialog('destroy');
+						newAlert(msg);
+					}
+			)
+		});
+		$("#quemsomos_exibir").click(function(){
+			/*
+			if($(this).prop('checked')){
+				$("#curso_dt_ini").attr('disabled','disabled');
+				$("#curso_dt_fim").attr('disabled','disabled');
+			}else{
+				$("#curso_dt_ini").removeAttr('disabled');
+				$("#curso_dt_fim").removeAttr('disabled');
+			}*/
+		});
+		
+		$(".delImg").click(function(){
+			var obj = $(this);
+			var img = $(this).attr('rel');
+			var id = $("#curso_id").val();
+			
+			var param = {
+				'quemsomos_id':id
+				,'img':img
+			}
+			deleteItem(
+					"Deseja remover essa imagem?"
+					,"controller/quemSomos.controller.php?action=removeImage"
+					,param
+					,function(msg,oDialog){
+						oDialog.dialog('close');
+						oDialog.dialog('destroy');
+						obj.parent().parent().find('.images').remove();
+						obj.parent().remove();
+						newAlert(msg);
+					}
+					,function(msg,oDialog){
+						oDialog.dialog('close');
+						oDialog.dialog('destroy');
+						newAlert(msg);
+					}
+			)
+		});
 	});
 })(jQuery);
