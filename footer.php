7c05a7b846ef8e64f361f039e5d8e96a79f52889
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2014-11-06 13:42:55 +0000

    Implementações / Correções

--- footer.php
+++ footer.php
@@ -12,12 +12,15 @@
 	$objTipoCurso = new tipo_curso();
 	$objTipoProjeto = new tipoProjeto();
 
+	
 	$objContato->setValues(array(
 		'contato_exibir'=>'S'
 		,'page'=>'1'
 		,'rows'=>'10'
 	));
+
 	$aContato = $objContato->getLista();
+	//$objContato->debug($aContato);
 
 	$objTipoServico->setValues(array(
 		'page'=>'1'
@@ -45,7 +48,70 @@
 	));
 	$aQuemSomos = $objQuemSomos->getLista();
 	//$objQuemSomos->debug($aQuemSomos);
+
 	
+	/*Organizando os contados por categoria;
+	1 - Telefones e Celulares
+	2 - Emails e Sites
+	3 - Facebook
+	4 - Redes Sociais (Social Media)
+	
+	*/
+
+	echo substr('Social Media (Linkedin)',0,12);
+	
+	
+	$aCel=array();
+	$celPos=0;
+	$aSite=array();
+	$sitePos=0;
+	$aEmail = array();
+	$emailPos=0;
+	$aFacebook = array();
+	$facePos=0;
+	$aSkype = array();
+	$skypePos=0;
+	$aSocialMedia = array();
+	$socialPos=0;
+	$arr = array();
+	foreach($aContato['rows'] as $k => $v){
+		$tipo=strtolower($v['contato_tipo']);
+		
+		if($tipo=='celular' || $tipo=='telefone'){
+			$arr = $objContato->createContactGroup($v);
+			array_push($aCel,$arr);
+			$celPos++;
+		}else if($tipo=='skype'){
+			$arr = $objContato->createContactGroup($v);
+			array_push($aSkype,$arr);
+			$skypePos++;
+		}else if($tipo=='site'){
+			$arr = $objContato->createContactGroup($v);
+			array_push($aSite,$arr);
+			$sitePos++;
+		}else if($tipo=='e-mail'){
+			$arr = $objContato->createContactGroup($v);
+			array_push($aEmail,$arr);
+			$emailPos++;
+			
+		}else if($tipo=='facebook'){
+			$arr = $objContato->createContactGroup($v);
+			array_push($aFacebook,$arr);
+			$facePos++;
+		}else if(substr ($tipo, 0, 12)=='social media'){
+			$arr = $objContato->createContactGroup($v);
+			array_push($aSocialMedia,$arr);
+			$socialPos++;
+		}
+	}
+/*
+	$objContato->debug($aCel);
+	$objContato->debug($aEmail);
+	$objContato->debug($aSkype);
+	$objContato->debug($aSite);
+	$objContato->debug($aFacebook);
+	$objContato->debug($aSocialMedia);
+*/
 	
 ?>
 <div class="clear"></div>    
@@ -78,39 +144,64 @@
 ?>
 		
 			<div id="devices">
-				<?php 
-					foreach($devices as $k => $v){
-						$tipo=strtolower($v['contato_tipo']);
-						if($tipo=='celular' || $tipo=='telefone'){
-				?>	
-						<span tabIndex="" class="fone"><?=$v['contato_nome'];?></span>
-				<?php
-						}elseif($tipo=='skype'){
-				?>	
-						<span tabIndex="" class="skype"><?=$v['contato_nome'];?></span>
-				<?php
-						}else{
-				?>	
-						<a tabIndex="" class="facebook" href="<?=$v['contato_link'];?>" target="_BLANK"><?=$v['contato_nome'];?></a>
-				<?php
-						}
-					}
-				?>
-            </div>
-            <div id="web">
-				<?php 
-					foreach($sites as $k => $v){
-						$tipo=strtolower($v['contato_tipo']);
-						if($tipo=='e-mail'){
-							$link='mailto:';
-						}else{
-							$link='';
-						}
-				?>	
-				<a tabIndex="" href="<?=$link.$v['contato_link'];?>" target="_BLANK"><?=$v['contato_nome'];?></a>
-				<?php
-					}
-				?>	
+				<?php  foreach($aCel as $k => $v){ ?>
+					<span tabIndex="">
+						<img class="normal" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<img class="contrast" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone_contraste'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<?=$v['contato_nome'];?>
+					</span>
+				<?php } ?>
+
+				<?php  foreach($aSkype as $k => $v){ ?>
+					<span tabIndex="">
+						<br />
+						<img class="normal" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<img class="contrast" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone_contraste'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<?=$v['contato_nome'];?>
+					</span>
+				<?php } ?>
+
+				<?php  foreach($aFacebook as $k => $v){ ?>
+						<br />
+						<img class="normal" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<img class="contrast" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone_contraste'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<a tabIndex="" href="<?=$v['contato_link'];?>" target="_BLANK"><?=$v['contato_nome'];?></a>
+				<?php } ?>
+
+				<?php  foreach($aEmail as $k => $v){ ?>
+						<br />
+						<img class="normal" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<img class="contrast" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone_contraste'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<a tabIndex="" href="mailto:<?=$v['contato_link'];?>" target="_BLANK"><?=$v['contato_nome'];?></a>
+				<?php } ?>
+
+				<?php  foreach($aSite as $k => $v){ ?>
+						<br />
+						<img class="normal" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<img class="contrast" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone_contraste'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<a tabIndex="" href="<?=$v['contato_link'];?>" target="_BLANK"><?=$v['contato_nome'];?></a>
+				<?php } ?>
+					
+				<?php  foreach($aSocialMedia as $k => $v){ ?>
+						<br />
+						<img class="normal" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<img class="contrast" src="<?=$linkAbsolute;?>images/<?=$v['contato_tipo_icone_contraste'];?>" title="<?=$v['contato_nome'];?>" alt="<?=$v['contato_nome'];?>" />
+						<a tabIndex="" href="<?=$v['contato_link'];?>" target="_BLANK"><?=$v['contato_nome'];?></a>
+				<?php } ?>
+					
+				<!-- RSS -->
+				<a tabIndex="" class="rss" href="<?=$linkAbsolute;?>rss" target="_BLANK">RSS</a>					
+				<br />
+				
+				<!-- FAVORITOS -->
+				<a tabIndex="" class="favorite" href="<?=$linkAbsolute;?>rss" target="_BLANK">Inserir nos favoritos</a>					
+				<!-- IMPRIMIR -->
+				<a tabIndex="" class="print" href="<?=$linkAbsolute;?>rss" target="_BLANK">Imprimir</a>					
+				<!-- ENVIAR POR E-MAIL -->
+				<a tabIndex="" class="sendmail" href="<?=$linkAbsolute;?>rss" target="_BLANK">Enviar por e-mail</a>					
+				
+				
+				
             </div>
             <strong id="access-option"><a tabIndex="120" href="<?=$linkAbsolute;?>acessibilidade">atalhos de teclado para facilitar a navegação</a></strong>
         </div>
