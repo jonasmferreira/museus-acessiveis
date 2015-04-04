56607e352f98af7d9823b099194ed4c0b08b8135
joynilson@gmail.com <joynilson@gmail.com@041b3ac0-a505-30a6-059d-899ba43c32d4>
2015-04-04 00:17:36 +0000

    Alterações no Emkt

--- boxBusca.php
+++ boxBusca.php
@@ -1,7 +1,7 @@
-    	<form action="javascript:void(0);" method="POST" id="search">
-        	<h1 tabIndex="57">Busca</h1>
-            <p tabIndex="58" class="description"><i>Saiba mais sobre inclusão sem complicação!!!</i></p>
-            <input tabIndex="59" type="text" name="busca_texto"  id="busca_texto" class="field" value="" />
-			<input tabIndex="60" type="image" class="bt-search" src="img/search-bt_transparent.png" style="" />            
-			<div class="clear"></div>
-        </form>
+<form action="javascript:void(0);" method="POST" id="search">
+		<h1 tabIndex="57">Busca</h1>
+		<p tabIndex="58" class="description"><i>Saiba mais sobre inclusão sem complicação!!!</i></p>
+		<input tabIndex="59" type="text" name="busca_texto"  id="busca_texto" class="field" value="" />
+		<input alt="botão buscar" tabIndex="60" type="image" class="bt-search" src="img/search-bt_transparent.png" style="" />            
+		<div class="clear"></div>
+</form>
